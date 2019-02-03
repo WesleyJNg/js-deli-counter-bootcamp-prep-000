b4fa47008e939342bc6b01155bd932bd386f5340
Wesley Ng <wesjng+learnco@gmail.com>
2019-02-03 04:20:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ function takeANumber (parameter1, parameter2) {
   parameter1.push(parameter2);
   return "Welcome, "+parameter2+". You are number "+parameter1.length+" in line.";
 }
+
+function nowServing ()
