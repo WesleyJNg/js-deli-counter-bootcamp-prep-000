b06d8b1a9eaf31c4b1ac0c70a18d2e45012e922d
Wesley Ng <wesjng+learnco@gmail.com>
2019-02-03 05:15:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nowServing(katzDeliLine) {
     return 'There is nobody waiting to be served!';
   }
   else {
-    return 'Now serving '+katzDeliLine.shift();
+    return 'Currently serving '+katzDeliLine.shift();
   }
 }
 
